ab630cda926dcd7d808aad980c1c1e5f0a014f1b
Carlos <casalgado86@gmail.com>
2024-10-04 07:38:21 -0500

    demo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,16 +6,7 @@ import UserCard from "../components/home/UserCard.vue";
   <section>
     <h1>Home</h1>
     <p>Welcome to the Home page</p>
-    <div class="card-container">
-      <UserCard
-        :user="{
-          firstName: 'John',
-          lastName: 'Doe',
-          jobDescription: 'Software Developer',
-          totalBalance: 10000,
-        }"
-      />
-    </div>
+    <div class="card-container"></div>
   </section>
 </template>
 
