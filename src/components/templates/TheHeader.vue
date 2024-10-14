8e29dbe79de6f408b50e51793924490628f3f91e
Carlos <casalgado86@gmail.com>
2024-10-14 11:34:37 -0500

    nav

--- src/components/templates/TheHeader.vue
+++ src/components/templates/TheHeader.vue
@@ -1,9 +1,34 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
 
 <template>
   <header>
-   
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/dashboard">Dashboard</RouterLink>
+    </nav>
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+nav {
+  padding: 1rem;
+
+  a {
+    font-weight: bold;
+    color: var(--background-blue);
+    text-decoration: none;
+    margin: 0 0.5rem;
+    transition: color 0.3s ease;
+
+    &:hover {
+      color: var(--light-blue);
+    }
+
+    &.router-link-exact-active {
+      color: var(--dark-blue);
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,16 @@ import UserCard from "../components/home/UserCard.vue";
   <section>
     <h1>Home</h1>
     <p>Welcome to the Home page</p>
-    <div class="card-container"></div>
+    <div class="card-container">
+      <UserCard
+        :user="{
+          firstName: 'John',
+          lastName: 'Doe',
+          jobDescription: 'Software Developer',
+          totalBalance: 10000,
+        }"
+      />
+    </div>
   </section>
 </template>
 
